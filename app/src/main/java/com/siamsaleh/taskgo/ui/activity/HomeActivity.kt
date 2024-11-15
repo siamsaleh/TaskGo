5db159d97cfd85177cb2b02a8e41dd355608cd0e
siamsaleh <siamsaleh9@gmail.com>
2024-11-16 00:31:40 +0600

    feat: HomePage Design Appbar, Search, Popular Category, BottomNav
    - Create HomeFragment
    
    - Add gilroy_medium and rubik font
    
    Create dimens.xml file
    
    - Customize status bar color and primary secondary color
    
    - Create style.xml file and Add currentLocationTextStyle, TitleTextStyle, CategoryTextStyle
    
    - Add Icons for homePage UI
    
    - Create bottom_nav_menu.xml to implement BottomNavigationView

--- app/src/main/java/com/siamsaleh/taskgo/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/siamsaleh/taskgo/ui/activity/HomeActivity.kt
@@ -1,21 +1,31 @@
 package com.siamsaleh.taskgo.ui.activity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.siamsaleh.taskgo.R
+import com.siamsaleh.taskgo.databinding.ActivityHomeBinding
+import com.siamsaleh.taskgo.ui.fragment.HomeFragment
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Load the default fragment
+        loadDefaultFragment(savedInstanceState)
+    }
+
+    private fun loadDefaultFragment(savedInstanceState: Bundle?) {
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment())
+                .commit()
         }
     }
 }
